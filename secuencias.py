489cf8dedbad785dbc702de45e591d0ad3d94e7f
romanaaronfr300-wq <romanaaronfr300@gmail.com>
2026-07-23 17:21:15 -0600

    secuencia de numeros

---
+++ secuencias.py
@@ -0,0 +1,11 @@
+#Secuencia de cuadros
+N = int(input("Numero positivo: "))
+i = 1
+
+while True:
+    print(i**2)
+    i += 1
+    if i > N:
+        break
+    
+print("Secuencia de cuadros hasta", N)
