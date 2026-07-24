7bb0240d65295de7dcb44d62e8c7589179f0948c
romanaaronfr300-wq <romanaaronfr300@gmail.com>
2026-07-23 19:39:24 -0600

    hacer un listado y seleccionar los numeros que dividan 3 y 5

---
+++ contador_divisor.py
@@ -0,0 +1,6 @@
+#Multiplos de 3 y 5
+print("Números divisibles por 3 y 5 (1-100):")
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i, end=" ")
+print()  # salto de línea final
