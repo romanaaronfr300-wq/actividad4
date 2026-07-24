d34c59ad46b87efa906d096dd197fc86867e187c
romanaaronfr300-wq <romanaaronfr300@gmail.com>
2026-07-23 23:10:02 -0600

    es una calculadora basica

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+#Calculadora basica
+while True:
+ print("1.Suma 2.Resta 3.Multiplicacion 4.Division 5.Salir")
+ op = int(input("Elige una opcion: "))
+ if op == 5:
+    break
+ a = float(input("Primer numero: "))
+ b = float(input("segundo numero: "))
+ match op:
+    case 1: print(a + b)
+    case 2: print(a - b)
+    case 3:  print(a * b)
+    case 4:
+        if b != 0:
+           print(a / b)
+        else:
+           print("Error: Division por cero")
+        resp = input("Desea continuar? (s/n): ") .lower()
+        if resp == 'n':
+           break
