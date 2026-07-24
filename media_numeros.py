36200edf820401d8a0db9d9fdda4932e7bc0d3e9
romanaaronfr300-wq <romanaaronfr300@gmail.com>
2026-07-24 00:35:36 -0600

    saca la media de numeros positivos

---
+++ media_numeros.py
@@ -0,0 +1,15 @@
+#Media de numeros positivos
+suma = 0
+contador = 0
+while True:
+    num = float(input("Número positivo (negativo sale): "))
+    if num < 0:
+        break
+    if num > 0:
+        suma += num
+        contador += 1
+if contador > 0:
+    media = suma / contador
+    print("Media:", media)
+else:
+    print("No se ingresaron positivos")
